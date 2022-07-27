0d8b5527d607518b463b52e15e753141ef6d4336
Berkut4713 <berkut_bm@mail.ru>
2022-07-28 00:35:03 +0300

    feat: add index.html 2

--- src/index.js
+++ src/index.js
@@ -2,5 +2,22 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  /*function towelSort(matrix) {*/
+  let lines = matrix.length;
+  let columns = matrix[0].length;
+  let mass = [];
+  for (let i = 0; i < lines; i++) {
+    if (i%2 !== 0) {
+        mass = mass.concat(matrix[i].reverse());
+    } else {
+        mass = mass.concat(matrix[i]);
+    }
+  }
+  return mass;
 }
+towelSort ([
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [9, 6, 4],
+  ]);
